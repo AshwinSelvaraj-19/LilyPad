cfa69a4923d3cc49c5c7c00db4fb0ddefee66aac
Ashwin Selvaraj <ashwinselvaraj19@gmail.com>
2026-06-12 11:14:07 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Developed By Phoenix",
         // Activity type number (0 = Playing).
         type: 0, 
       },
