3a13e2e678dc37216b12d9d55ab038be8ae7184a
Ashwin Selvaraj <ashwinselvaraj19@gmail.com>
2026-06-12 12:14:34 +0530

    Add 24/7 voice channel join

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,7 @@ import { Events } from "discord.js";
 import { logger, startupLog } from "../utils/logger.js";
 import config from "../config/application.js";
 import { reconcileReactionRoleMessages } from "../services/reactionRoleService.js";
+import { joinVoiceChannel } from '@discordjs/voice';
 
 export default {
   name: Events.ClientReady,
@@ -14,6 +15,20 @@ export default {
       startupLog(`Ready! Logged in as ${client.user.tag}`);
       startupLog(`Serving ${client.guilds.cache.size} guild(s)`);
       startupLog(`Loaded ${client.commands.size} commands`);
+      
+      const guild = client.guilds.cache.get('1502923562506256407');
+      const channel = guild?.channels.cache.get('1502962402654818335');
+      
+if (channel) {
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+  });
+
+  startupLog(`Joined voice channel: ${channel.name}`);
+}
 
       const reconciliationSummary = await reconcileReactionRoleMessages(client);
       startupLog(
